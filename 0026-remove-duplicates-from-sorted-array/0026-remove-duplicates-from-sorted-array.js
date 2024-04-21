b9a0515d4e7ce44e7c235a9ec53bee640ee4e737
Jeffrey Vaughn <75887112+jvaughn1230@users.noreply.github.com>
2024-04-21 14:58:39 -0700

    Time: 52 ms (95.73%), Space: 52.8 MB (20.50%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return 1;
+    
+     let slow = 0;
+    
+    for (let fast = 1; fast < nums.length; fast++) {
+     
+        if (nums[fast] !== nums[slow]) {
+            slow++;
+            nums[slow] = nums[fast];
+        }
+    }
+    
+
+    return slow + 1;
+    
+    
+};
