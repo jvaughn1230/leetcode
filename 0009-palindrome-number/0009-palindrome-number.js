6267b96ed2bbf7f5ed15d950fe00a1d8dfca4305
Jeffrey Vaughn <75887112+jvaughn1230@users.noreply.github.com>
2024-04-22 16:56:56 -0700

    Time: 124 ms (82.01%), Space: 58.3 MB (41.88%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,8 +3,5 @@
  * @return {boolean}
  */
 const isPalindrome = (x) => {
-    if (x.toString() === x.toString().split('') .reverse().join('')) return true;
-    
-    return false
-    
+ return x.toString() === x.toString().split('') .reverse().join('') ?  true :  false;
 }   
