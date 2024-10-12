557aaa4dbbb17a6348540776d040a1ea9d1d862a
Jeffrey Vaughn <75887112+jvaughn1230@users.noreply.github.com>
2024-10-11 21:58:45 -0700

    Time: 63 ms (11.56%), Space: 49.1 MB (82.33%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let mergedString = [];
+    
+    for(let i=0; i < word1.length || i < word2.length; i++){
+        if(i <= word1.length){
+            mergedString.push(word1[i]);
+        }
+         if(i <= word2.length){
+            mergedString.push(word2[i]);
+        }
+    }
+    
+    return mergedString.join('');
+};
